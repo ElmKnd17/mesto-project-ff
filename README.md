8a4314ab929ec09ca9253c59dc6242cb3b47c125
Elman Magerramov <elmanmagerramov63elman@yandex.ru>
2024-02-25 15:25:21 +0400

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/ElmKnd17/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const template = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCards (initialCard) {
+    const cardItem = template.content.cloneNode(true);
+    cardItem.querySelector('.card__image').setAttribute('src', initialCard.link);
+    cardItem.querySelector('.card__title').textContent = initialCard.name;
+    cardItem.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    cardList.append(cardItem);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const eventTarget = evt.target;
+    const cardItem = eventTarget.closest('.places__item');
+    cardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(element => {
+    addCards(element);
+})
